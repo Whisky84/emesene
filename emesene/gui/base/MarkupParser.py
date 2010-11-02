6d8f1af88b48db3e8095a7050b5c1ec5950439c6
Gabriele "Whisky" Visconti <fastfading@mercurius.(none)>
2010-11-02 03:58:37 +0100

    qt4: updated to reflect upstream changes

--- emesene/gui/base/MarkupParser.py
+++ emesene/gui/base/MarkupParser.py
@@ -24,7 +24,6 @@ def unescape(string_):
     '''replace the values on dic_inv keys with the values'''
     return xml.sax.saxutils.unescape(string_, dic_inv)
 
-# FIXME: update upstream
 def parse_emotes(message, cedict={}):
     '''parser the emotes in a message, return a string with img tags
     for the emotes acording to the theme'''
@@ -48,7 +47,7 @@ def parse_emotes(message, cedict={}):
 
             if len(parts) > 1:
                 if shortcut in gui.Theme.EMOTES.keys():
-                    path = gui.theme.emote_to_path(shortcut, True)
+                    path = gui.theme.emote_to_path(shortcut)
                 else:
                     path = cedict[shortcut]
                 tag = '<img src="%s" alt="%s"/>' % (path, shortcut)
